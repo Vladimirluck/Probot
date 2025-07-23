14cdaec58aa32de41092777ed83516c63f45e6f9
Vladimirluck <vladimir.luck113@gmail.com>
2025-07-23 21:28:29 +0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -4,7 +4,18 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # Нужно для SQLite
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)

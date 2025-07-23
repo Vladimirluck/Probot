bb0c22256fbbff49d9c0b6920a870972ecf62aae
Vladimirluck <vladimir.luck113@gmail.com>
2025-07-23 19:03:11 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from config import BOT_TOKEN
+from handlers import start
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+start.register_handlers(dp)
+
+if __name__ == '__main__':
+    print("🤖 ProBot запущен")
+    executor.start_polling(dp, skip_updates=True)
